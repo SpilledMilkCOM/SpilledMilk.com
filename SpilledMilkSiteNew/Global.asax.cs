8dcec67eb6e5817672d532fc962f4c2b193cc878
Parker Smart <psmart@spilledmilk.com>
2017-07-03 12:20:07 -0600

    Added Application_Error to handle 404 errors, but says that it's a controller error.

--- SpilledMilkSiteNew/Global.asax.cs
+++ SpilledMilkSiteNew/Global.asax.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -8,7 +6,7 @@ using System.Web.Routing;
 
 namespace SpilledMilkSiteNew
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -18,6 +16,31 @@ namespace SpilledMilkSiteNew
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        /// <summary>
+        /// NOTE: https://forums.asp.net/t/1505777.aspx?Error+Handling+in+global+asax
+        /// </summary>
+        protected void Application_Error()
+        {
+            var context = HttpContext.Current;
+            var lastError = context.Server.GetLastError();
+            var error = new KeyValuePair<string, object>("ErrorMessage", lastError.Message);
+            var stack = new KeyValuePair<string, object>("StackTrace", lastError.StackTrace);
+
+            context.Response.Clear();
+
+            RequestContext rc = ((MvcHandler)context.CurrentHandler).RequestContext;
+            string controllerName = rc.RouteData.GetRequiredString("controller");
+            IControllerFactory factory = ControllerBuilder.Current.GetControllerFactory();
+            IController controller = factory.CreateController(rc, controllerName);
+            ControllerContext cc = new ControllerContext(rc, (ControllerBase)controller);
+
+            ViewResult viewResult = new ViewResult { ViewName = "Error" };
+            viewResult.ViewData.Add(error);
+            viewResult.ExecuteResult(cc);
+            context.Server.ClearError();
+            //ctx.Response.End();
         }
     }
 }
